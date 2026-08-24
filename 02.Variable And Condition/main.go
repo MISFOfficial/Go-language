6defa405ae807c62b5bc07ed712528f8dfa93153
muksituljahin <muksitul.inno@pakizasoftware.com>
2026-08-24 11:06:26 +0600

    feat: add conditional logic demonstration for integer comparison in main.go

--- 02.Variable And Condition/main.go
+++ 02.Variable And Condition/main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	a := 100
-	fmt.Println(a)
+	a := 10
+	// fmt.Println(a)
+
+	if a > 100 {
+		fmt.Println(false)
+	} else if a == 100 {
+		fmt.Println(true)
+	} else {
+		fmt.Println("Tmr dara programming hobe na")
+	}
 }
