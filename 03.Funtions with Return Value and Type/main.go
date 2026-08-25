d08158c61f8dd31c3cf0e195e7da591e6e771056
muksituljahin <muksitul.inno@pakizasoftware.com>
2026-08-25 10:19:58 +0600

    feat: implement functions with multiple return values and specific return types

--- 03.Funtions with Return Value and Type/main.go
+++ 03.Funtions with Return Value and Type/main.go
@@ -12,4 +12,14 @@ func main() {
 	fmt.Println("Funtions and Return Value and Type")
 	sum := addition(10, 20)
 	fmt.Println("Addition result:", sum)
+
+	temp, rainfall := CelsiusToFahrenheit(25)
+	fmt.Println("Temperature:", temp, "Fahrenheit")
+	fmt.Println("Rainfall:", rainfall, "Inches")
+}
+
+func CelsiusToFahrenheit(celsius float64) (float64, float64) {
+	fahrenheit := (celsius * 9 / 5) + 32
+	rainfall := celsius * 0.0393701 // Convert mm to inches
+	return fahrenheit, rainfall
 }
